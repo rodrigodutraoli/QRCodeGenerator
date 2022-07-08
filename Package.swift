15123b7868deb2bc4ea9ce05394898dd45bba31c
Rodrigo Dutra de Oliveira <rodrigo@bravecl.com>
2022-07-07 21:33:09 -0300

    Swift version

--- Package.swift
+++ Package.swift
@@ -27,5 +27,6 @@ let package = Package(
         .testTarget(
             name: "QRCodeGeneratorTests",
             dependencies: ["QRCodeGenerator"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
